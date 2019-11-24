b1e9b1ae09b94f3623d1fd8c4417236cd900efdf
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2019-11-24 11:00:21 -0800

    Shorten logging again.

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ if (request.method == "GET") {
 			response.end($keyCallback+":scores:"+JSON.stringify($PagesResults))
 
 	}).catch(function(err) {
-		writeLog("Invalid select return attempt - UPDATE starsparLocations SET ticksremaining=100 WHERE objectName='"+$user+"';SELECT * FROM starsparLocations WHERE mapname = '"+map.name+"' AND ticksremaining > 0 AND locX > "+player.x+"-2000 AND "+player.x+"+2000 > locX AND locY > "+player.y+"-2000 AND "+player.y+"+2000 > locY OR mapname = '"+map.name+"' AND objectName = 'demon' ; - " + err.message)
+		writeLog("Invalid select return attempt - UPDATE objectName='"+$user+"';SELECT  mapname = '"+map.name+"' AND  locX > "+player.x+"-2000 AND "+player.x+"+2000 > locX AND locY > "+player.y+"-2000 AND "+player.y+"+2000 > locY OR mapname = '"+map.name+"' AND objectName = 'demon' ; - " + err.message)
 		console.log("Invalid select return attempt")
 	})//end Pages query
 	} else {
