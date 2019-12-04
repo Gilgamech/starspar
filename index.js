062bcddbefcc8afc44793bea7be3a65fe6d885ef
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2019-12-03 18:51:09 -0800

    Re-simplify movement calls.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //StarSpar server file.
 //(c) 2019 Gilgamech Technologies
 var $gameData = {};
-$gameData.ver = 294
+$gameData.ver = 293
 
 //{ Init vars
 var $http = require("http");
@@ -137,14 +137,14 @@ function gameTick() {
 	}
 	
     for (object in $gameObjects.filter(o => {return o.objecttype != 'block'})) {
-		if ($gameObjects[object].objectType == 'player' || $gameObjects[object].objecttype == 'player') { //if player 
+		if ($gameObjects[object].objecttype == 'player') { //if player 
 			$gameObjects[object].ticksremaining--
-		}else if ($gameObjects[object].objectType == 'npc' || $gameObjects[object].objecttype == 'npc') { //if demon 
+		}else if ($gameObjects[object].objecttype == 'npc') { //if demon 
 			moveObject($gameObjects[object])
-		}else if ($gameObjects[object].objectType == 'projectile' || $gameObjects[object].objecttype == 'projectile') { //if prjectile
+		}else if ($gameObjects[object].objecttype == 'projectile') { //if prjectile
 			$gameObjects[object].hp--
 			moveObject($gameObjects[object])
-		}else if ($gameObjects[object].objectType == 'ammo' || $gameObjects[object].objecttype == 'ammo') { //if projectile 
+		}else if ($gameObjects[object].objecttype == 'ammo') { //if projectile 
 			$gameObjects[object].hp--
 			moveObject($gameObjects[object])
 		}    
